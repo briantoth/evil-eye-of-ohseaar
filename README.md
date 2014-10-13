7ac4eb330b4569d13b2d80e8ccc3aafb0a4a5338
Brian Toth <bri.toth@gmail.com>
2014-10-12 21:45:25 -0700

    Full loop!  Issues exist with cropping though.

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 #Dependencies
 
-Whoosh - https://pypi.python.org/pypi/Whoosh/
 FuzzyWuzzy - https://github.com/seatgeek/fuzzywuzzy
 tesseract-orc - https://code.google.com/p/tesseract-ocr/
+opencv and python bindings - apt-get install python-opencv
 
 ##Optional
 Levenshtein - pip install python-levenshtein (makes the fuzzywuzzy run faster)
--- build_search_index.py
+++ build_search_index.py
@@ -4,11 +4,25 @@ import json
 from pprint import pprint
 from fuzzywuzzy import process
 import os
+import cv2
 
 VERSION_FILE = 'data/version.txt'
 DATA_FILE = 'data/all-sets.json'
 TEMP_FILE = 'tmp'
+TEMP_FULL_TEXT_FILE = 'tmp-full-text'
 INPUT_DIRECTORY = 'input'
+CROPPED_DIRECTORY = 'cropped'
+
+
+def check_output(tmp_file):
+    success = True
+    with open(tmp_file, 'r') as f:
+        found_text = f.read().strip()
+        print "Found text:" + found_text
+        if len(found_text) == 0:  # error condition
+            success = False
+
+        return (success, found_text)
 
 # check to see if stored version of card data is outdated
 new_version = urllib2.urlopen('http://mtgjson.com/json/version.json').read()
@@ -40,18 +54,42 @@ for card_set in all_cards.values():
     for card in card_set["cards"]:
         all_unique_card_names.add(card["name"])
 
-pprint(process.extract('Shittestrm', list(all_unique_card_names), limit=3))
-pprint(process.extract('Viashinoa Skanktile', list(all_unique_card_names), limit=3))
-
-#set the language location
+# set the language location
 os.environ['TESSDATA_PREFIX'] = '/home/brian/dev/tesseract-ocr-3.02'
 
-#attempt to identify each card
+failed_to_identify = []
+
+# attempt to identify each card
 for card in os.listdir(INPUT_DIRECTORY):
-    os.system('tesseract ' + INPUT_DIRECTORY + '/' + card  + TEMP_FILE)
+    if card == '.gitignore':
+        continue
 
-    with open(TEMP_FILE + '.txt', 'r') as f:
-        found_text = f.read().strip()
-        print found_text
+    card_path = INPUT_DIRECTORY + '/' + card
+    print card_path
+    image = cv2.imread(card_path)
+    crop_image = image[100:300, 125:1300]
+    cropped_path = CROPPED_DIRECTORY + '/' + card
+    cv2.imwrite(cropped_path, crop_image)
 
-    pprint(process.extract(found_text, list(all_unique_card_names), limit=3))
+    os.system('tesseract ' + cropped_path + ' ' + TEMP_FILE)
+
+    tmp_file = TEMP_FILE + '.txt'
+    result = check_output(tmp_file)
+    full_text = ''
+
+    if not result[0]:  # error condition, retry
+        os.system('tesseract -psm 7 ' + cropped_path + ' ' + TEMP_FILE)
+        result = check_output(tmp_file)
+
+    #os.system('tesseract ' + card_path + ' ' + TEMP_FULL_TEXT_FILE)
+    #full_text = check_output(TEMP_FULL_TEXT_FILE + '.txt')
+
+    possible = process.extract(result[1], list(all_unique_card_names), limit=3)
+    if possible[0][1] < 70:
+        failed_to_identify.append([cropped_path, result[1]])
+    else:
+        pprint(possible[0])
+        #pprint(full_text)
+
+print "Failures:"
+pprint(failed_to_identify)
